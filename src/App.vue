b4a63a298e12a89c7a1a0c0e655356060d6ac6e1
huangfuxicheng <1766122474@qq.com>
2023-11-08 22:29:16 +0800

    测试husky

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <h1>APP</h1>
-      <h2>测试husky</h2>
+    <h2>测试husky</h2>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
