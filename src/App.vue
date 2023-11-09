ed166726aff41ff573453543cfd568b9c6ba2841
huangfuxicheng <1766122474@qq.com>
2023-11-09 15:19:39 +0800

    feat: 相对路径别名配置，使用 @ 代替 src

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import {Delete, Edit} from '@element-plus/icons-vue'
+</script>
 
 <template>
   <div>
-    <h1>APP</h1>
-    <h2>测试husky</h2>
-    <h2>测试husky</h2>
-    <h2>测试husky</h2>
-    <h2>测试husky</h2>
-    <h2>测试husky</h2>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    <el-button :icon="Delete" circle type="danger"/>
+    <el-button :icon="Edit" type="info"/>
+    <div class="example-pagination-block">
+      <div class="example-demonstration">When you have more than 7 pages</div>
+      <el-pagination :total="1000" layout="prev, pager, next"/>
+    </div>
   </div>
 </template>
 
