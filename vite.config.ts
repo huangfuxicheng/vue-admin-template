730616c462b16707d8e47bf81f6b6afb5a1f0645
huangfuxicheng <1766122474@qq.com>
2023-11-09 17:31:22 +0800

    feat: 自定义安装
    Signed-off-by: huangfuxicheng <1766122474@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        createSvgIconsPlugin({
-            // Specify the icon folder to be cached
-            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-            // Specify symbolId format
-            symbolId: 'icon-[dir]-[name]',
-        }),
-    ],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
+  ],
   resolve: {
     alias: {
       // 相对路径别名配置，使用 @ 代替 src
