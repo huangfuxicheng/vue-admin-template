7ed2496c2441d3ed130ca5767bf62dc19349d3a0
huangfuxicheng <1766122474@qq.com>
2023-11-14 17:31:39 +0800

    feat: layout
    Signed-off-by: huangfuxicheng <1766122474@qq.com>

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,6 +3,28 @@
     <!--   左侧菜单-->
     <div class="layout_slider">
       <logo></logo>
+      <el-scrollbar class="scrollbar">
+        <el-menu
+          active-text-color="#ffd04b"
+          background-color="#001529"
+          class="el-menu-vertical-demo"
+          default-active="2"
+          text-color="#fff"
+        >
+          <el-menu-item index="1">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <span>首页</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <span>数据大屏</span>
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!--    顶部导航-->
     <div class="layout_tabbar">2</div>
@@ -13,6 +35,7 @@
 
 <script setup lang="ts">
 import logo from '@/layout/logo/index.vue'
+import { Setting } from '@element-plus/icons-vue'
 </script>
 
 <style scoped lang="scss">
@@ -20,11 +43,18 @@ import logo from '@/layout/logo/index.vue'
   width: 100%;
   height: 100vh;
   background-color: blue;
+
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-logo-height);
+    }
   }
+
   .layout_tabbar {
     position: fixed;
     width: calc(100% - $base-menu-width);
@@ -33,6 +63,7 @@ import logo from '@/layout/logo/index.vue'
     top: 0;
     left: $base-menu-width;
   }
+
   .layout_main {
     position: absolute;
     width: calc(100% - $base-menu-width);
