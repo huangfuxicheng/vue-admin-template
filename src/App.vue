9e1fba697a6818a4f83113f94b28cf9e8b843c36
huangfuxicheng <1766122474@qq.com>
2023-11-21 22:27:53 +0800

    feat: refresh

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,31 @@
 //     password: '111111',
 //   })
 // })
+import useLayOutSettingStore from '@/store/modules/setting'
+import { nextTick, ref, watch } from 'vue'
+
+let layOutSettingStore = useLayOutSettingStore()
+let flag = ref(true)
+//监听仓库内部的数据是否发生变化，如果发生变化，说明用户点击过刷新按钮
+watch(
+  () => layOutSettingStore.refresh,
+  () => {
+    //点击刷新按钮：路由组件销毁
+    flag.value = false
+    nextTick(() => {
+      flag.value = true
+    })
+  },
+)
 </script>
 
 <template>
   <div>
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <component :is="Component" v-if="flag"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
