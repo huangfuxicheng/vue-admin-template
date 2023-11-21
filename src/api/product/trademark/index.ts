0401e930e270a9579de92cbae234a7ad469c8d34
huangfuxicheng <1766122474@qq.com>
2023-11-21 17:26:30 +0800

    feat: trademark请求
    Signed-off-by: huangfuxicheng <1766122474@qq.com>

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,1 +1,8 @@
-enum API {}
+import request from '@/utils/request.ts'
+
+enum API {
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
+}
+
+export const getTradeMark = (page: number, size: number) =>
+  request.get<any, any>(API.TRADEMARK_URL + `${page}/${size}`)
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,31 +1,59 @@
 <template>
   <el-card>
     <el-button type="primary" size="default" icon="Plus">添加品牌</el-button>
-    <el-table style="margin: 10px 0" border>
+    <el-table style="margin: 10px 0" border :data="trademarkArr">
       <el-table-column
         label="序号"
         width="80px"
         align="center"
+        type="index"
       ></el-table-column>
-      <el-table-column label="品牌名称"></el-table-column>
-      <el-table-column label="品牌LOGO"></el-table-column>
-      <el-table-column label="品牌操作"></el-table-column>
+      <el-table-column label="品牌名称" prop="tmName"></el-table-column>
+      <el-table-column label="品牌LOGO">
+        <template #="{ row, $index }">
+          <img
+            :src="row.logoUrl"
+            alt="logo"
+            style="width: 50px; height: 50px"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column label="品牌操作">
+        <template #="{ row, $index }">
+          <el-button type="primary" size="small" icon="Edit"></el-button>
+          <el-button type="primary" size="small" icon="Delete"></el-button>
+        </template>
+      </el-table-column>
     </el-table>
     <el-pagination
-      v-model:current-page="currentPage4"
-      v-model:page-size="pageSize4"
+      v-model:current-page="currentPage"
+      v-model:page-size="pageSize"
       :page-sizes="[3, 5, 7, 9]"
-      layout=" prev, pager, next, jumper,->,sizes, total"
-      :total="400"
+      layout=" prev, pager, next,jumper, ->, sizes, total"
+      :total="total"
     />
   </el-card>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
+import { getTradeMark } from '@/api/product/trademark'
 
-const currentPage4 = ref(1)
-const pageSize4 = ref(3)
+let currentPage = ref<number>(1)
+let pageSize = ref<number>(3)
+let total = ref<number>(0)
+let trademarkArr = reactive([])
+const handleSubmit = async () => {
+  const resp: any = await getTradeMark(currentPage.value, pageSize.value)
+  if (resp.code === 200) {
+    total.value = resp.data.total
+    trademarkArr = resp.data.records
+  }
+}
+
+onMounted(() => {
+  handleSubmit()
+})
 </script>
 
 <style scoped></style>
