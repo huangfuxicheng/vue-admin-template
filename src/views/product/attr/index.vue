f826e35142ad8ba1c92e10a91c52e6e5321f29b6
huangfuxicheng <1766122474@qq.com>
2023-11-23 17:30:11 +0800

    feat: attr
    Signed-off-by: huangfuxicheng <1766122474@qq.com>

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,5 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <el-card>
+      <el-form inline>
+        <el-form-item label="一级分类">
+          <el-select>
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="二级分类">
+          <el-select>
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="三级分类">
+          <el-select>
+            <el-option></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card style="margin: 10px 0">12</el-card>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
