97935f5a9ecbad136602f22355f3218038a27961
huangfuxicheng <1766122474@qq.com>
2023-11-23 20:59:07 +0800

    fix: 修改图标注册位置

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,15 @@
 import SvgIcon from './SvgIcon/index.vue'
+import Category from './Category/index.vue'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import type { App, Component } from 'vue'
-const components: { [name: string]: Component } = { SvgIcon }
+const components: { [name: string]: Component } = { SvgIcon, Category }
 export default {
   install(app: App) {
     Object.keys(components).forEach((key: string) => {
       app.component(key, components[key])
     })
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component)
+    }
   },
 }
--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,9 @@ import 'virtual:svg-icons-register'
 import globalComponent from '@/components/index'
 import '@/styles/index.scss'
 import pinia from '@/store'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
+
 app.use(ElementPlus, {
   locale: zhCn,
 })
