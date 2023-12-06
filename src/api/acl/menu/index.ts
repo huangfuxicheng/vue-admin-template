0f3cec1949fdb3c968310ff846341f2d81881ac5
huangfuxicheng <1766122474@qq.com>
2023-12-06 22:55:14 +0800

    feat: menu完成
    Signed-off-by: huangfuxicheng <1766122474@qq.com>

--- src/api/acl/menu/index.ts
+++ src/api/acl/menu/index.ts
@@ -1,11 +1,30 @@
 import request from '@/utils/request'
-import type { PermisstionResponseData, MenuParams } from './type'
+import type { MenuParams, PermisstionResponseData } from './type'
 //枚举地址
 enum API {
   //获取全部菜单与按钮的标识数据
   ALL_PERMISSION_URL = '/admin/acl/permission',
+  //给某一级菜单新增一个子菜单
+  ADD_MENU_URL = '/admin/acl/permission/save',
+  //更新某一个已有的菜单
+  UPDATE_URL = '/admin/acl/permission/update',
+  //删除已有的菜单
+  DELETE_MENU_URL = '/admin/acl/permission/remove/',
 }
 //获取菜单数据
 export const reqAllPermisstion = () => {
   return request.get<any, PermisstionResponseData>(API.ALL_PERMISSION_URL)
+}
+//添加与更新菜单的方法
+export const reqAddOrUpdateMenu = (data: MenuParams) => {
+  if (data.id) {
+    return request.put<any, any>(API.UPDATE_URL, data)
+  } else {
+    return request.post<any, any>(API.ADD_MENU_URL, data)
+  }
+}
+
+//删除某一个已有的菜单
+export const reqRemoveMenu = (id: number) => {
+  return request.delete<any, any>(API.DELETE_MENU_URL + id)
 }
--- src/views/acl/permission/index.vue
+++ src/views/acl/permission/index.vue
@@ -1,7 +1,149 @@
 <template>
-  <div>111</div>
+  <div>
+    <el-table
+      :data="PermisstionArr"
+      style="width: 100%; margin-bottom: 20px"
+      row-key="id"
+      border
+      default-expand-all
+    >
+      <el-table-column label="名称" prop="name"></el-table-column>
+      <el-table-column label="权限值" prop="code"></el-table-column>
+      <el-table-column label="修改时间" prop="updateTime"></el-table-column>
+      <el-table-column label="操作">
+        <!-- row:即为已有的菜单对象|按钮的对象的数据 -->
+        <template #default="{ row }">
+          <el-button
+            type="primary"
+            size="small"
+            :disabled="row.level == 4"
+            @click="addPermisstion(row)"
+          >
+            {{ row.level == 3 ? '添加功能' : '添加菜单' }}
+          </el-button>
+          <el-button
+            type="primary"
+            size="small"
+            :disabled="row.level == 1"
+            @click="updatePermisstion(row)"
+          >
+            编辑
+          </el-button>
+          <el-popconfirm
+            :title="`你确定要删除${row.name}?`"
+            width="260px"
+            @confirm="removeMenu(row.id)"
+          >
+            <template #reference>
+              <el-button type="primary" size="small" :disabled="row.level == 1">
+                删除
+              </el-button>
+            </template>
+          </el-popconfirm>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog
+      v-model="dialogVisible"
+      :title="menuData.id ? '更新菜单' : '添加菜单'"
+    >
+      <el-form>
+        <el-form-item label="品牌">
+          <el-input placeholder="请输入" v-model="menuData.name"></el-input>
+        </el-form-item>
+        <el-form-item label="权限值">
+          <el-input placeholder="请输入" v-model="menuData.code"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="save">确定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//存储菜单的数据
+import { onMounted, reactive, ref } from 'vue'
+import {
+  reqAddOrUpdateMenu,
+  reqAllPermisstion,
+  reqRemoveMenu,
+} from '@/api/acl/menu'
+import { MenuParams, Permission, PermissionList } from '@/api/acl/menu/type.ts'
+import { ElMessage } from 'element-plus'
+
+let PermisstionArr = ref<PermissionList>([])
+let dialogVisible = ref<boolean>(false)
+let menuData = reactive<MenuParams>({
+  code: '',
+  level: 0,
+  name: '',
+  pid: 0,
+})
+//组件挂载完毕
+onMounted(() => {
+  getHasPermisstion()
+})
+//获取菜单数据的方法
+const getHasPermisstion = async () => {
+  let result = await reqAllPermisstion()
+  if (result.code == 200) {
+    PermisstionArr.value = result.data
+  }
+}
+
+//添加菜单按钮的回调
+const addPermisstion = (row: Permission) => {
+  //清空数据
+  Object.assign(menuData, {
+    id: 0,
+    code: '',
+    level: 0,
+    name: '',
+    pid: 0,
+  })
+  //对话框显示出来
+  dialogVisible.value = true
+  //收集新增的菜单的level数值
+  menuData.level = row.level + 1
+  //给谁新增子菜单
+  menuData.pid = row.id as number
+}
+//编辑已有的菜单
+const updatePermisstion = (row: Permission) => {
+  dialogVisible.value = true
+  //点击修改按钮:收集已有的菜单的数据进行更新
+  Object.assign(menuData, row)
+}
+//确定按钮的回调
+const save = async () => {
+  //发请求:新增子菜单|更新某一个已有的菜单的数据
+  let result = await reqAddOrUpdateMenu(menuData)
+  if (result.code == 200) {
+    //对话框隐藏
+    dialogVisible.value = false
+    //提示信息
+    ElMessage({
+      type: 'success',
+      message: menuData.id ? '更新成功' : '添加成功',
+    })
+    //再次获取全部最新的菜单的数据
+    getHasPermisstion()
+  }
+}
+
+//删除按钮回调
+const removeMenu = async (id: number) => {
+  let result = await reqRemoveMenu(id)
+  if (result.code == 200) {
+    ElMessage({ type: 'success', message: '删除成功' })
+    getHasPermisstion()
+  }
+}
+</script>
 
 <style scoped></style>
